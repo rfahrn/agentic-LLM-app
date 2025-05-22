567e8e4754ba5dcf8c6062ccf4852a5d10ef608c
rfahrn <rebecka.fahrni16@gmail.com>
2025-05-22 09:12:35 +0200

    sidebar expander

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,12 +29,21 @@ if page == "Apotheker Assistent":
         "Aktiviere externe Tools, welche der LLM Agent durchforsten soll – "
         "z. B. aus Compendium.ch, einer lokalen medizinischen Datenbank (pdf), EMAoder dem Web.\n\n"
     )
-    use_compendium = st.sidebar.checkbox("Compendium.ch", value=False)
-    use_internDB  = st.sidebar.checkbox("Local FAISS DB", value=False)
-    use_openfda   = st.sidebar.checkbox("OpenFDA", value=False)
-    use_web       = st.sidebar.checkbox("Web Search (Tavily)", value=True)
-    use_alerts    = st.sidebar.checkbox("Medication Alerts", value=False)
-    use_ema       = st.sidebar.checkbox("EMA", value=False)
+    st.sidebar.markdown("### 🛠️ Tools")
+    with st.sidebar.expander("Agenten-Tools aktivieren"):
+        st.checkbox("Compendium.ch", value=False)
+        st.checkbox("Local PDFs Database", value=False)
+        st.checkbox("OpenFDA", value=False)
+        st.checkbox("Open Web Search (Tavily)", value=True)
+        st.checkbox("Medication Alerts", value=False)
+        st.checkbox("EMA", value=False)
+        
+    #use_compendium = st.sidebar.checkbox("Compendium.ch", value=False)
+    #use_internDB  = st.sidebar.checkbox("Local FAISS DB", value=False)
+    #use_openfda   = st.sidebar.checkbox("OpenFDA", value=False)
+    #use_web       = st.sidebar.checkbox("Web Search (Tavily)", value=True)
+    #use_alerts    = st.sidebar.checkbox("Medication Alerts", value=False)
+    #use_ema       = st.sidebar.checkbox("EMA", value=False)
 
     # --- MAIN HEADER ---
     st.title(" KING – Apotheker Assistent")
